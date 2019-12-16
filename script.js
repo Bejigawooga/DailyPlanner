c6fd36bb89c8191b8c8d8214bbc0acc0464a9b07
Bejigawooga <55114061+Bejigawooga@users.noreply.github.com>
2019-12-15 19:35:36 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+var hour;
+function getHour(){
+$.ajax({
+    url:"http://worldclockapi.com/api/json/cst/now",
+    method: "GET"
+}).then(function(response){
+
+    console.log(response)
+
+    var dateString = response.currentDateTime;
+
+    hour = dateString.slice(11, 13);
+
+    setRowColors()
+
+})
+}
+
+function setRowColors(){
+
+    var rowNames = ["row1", "row2", "row3", "row4", "row5", "row6", "row7", "row8"];
+
+    for(var i=0; i<9;i++){
+        var rowString = "tr:eq(" + i + ")";
+        var cellTime=    $('#tableyy').find(rowString).find("td:eq(0)").html();
+        var rowStyle;
+
+        if (hour>cellTime){
+        $(rowNames[i]).addclass("past");
+    }
+        else if (hour===cellTime){
+            $(rowNames[i]).addclass("present");
+        }
+        else {
+            $(rowNames[i]).addclass("future");
+        }
+}
+
+
+
+
+
+
+
+}
+function localStore(){
+$(".input").html();
+localStorage.content= $(".input").html();
+$(".input").html(localStorage.content)
+
+}
+
+$(".localSave").click(localStore());
